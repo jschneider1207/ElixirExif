2e2e6195c8fab0328f453959c072062424477f97
Sam Schneider <sds6065@gmail.com>
2016-01-02 20:23:57 -0500

    fixed some fields getting overwritten

--- mix.exs
+++ mix.exs
@@ -3,30 +3,33 @@ defmodule ElixirExif.Mixfile do
 
   def project do
     [app: :elixir_exif,
-     version: "0.0.1",
-     elixir: "~> 1.1",
+     version: "0.1.0",
+     elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Parse exif and thumbnail data from jpeg/tiff images.
+    """
+  end
+
+  defp package do
+    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Sam Schneider"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/sschneider1207/ElixirExif"}]
   end
 end
